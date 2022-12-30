f70c8990d25b2c17b21ad1569fc7874eedf48d8a
Ken Ohkawa <knohkw@gmail.com>
2022-12-30 14:26:53 +0900

    added Input.vue

---
+++ src/components/Input.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    import { ref } from 'vue'
+    const text = ref("");
+
+    function clickButton() {
+        alert(text.value);
+    }
+</script>
+
+<template>
+    <p style="white-space: pre-line;">{{ text }}</p>
+    <textarea v-model="text"></textarea>
+    <button @click="clickButton">check</button>
+</template>
