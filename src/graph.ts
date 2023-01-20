2cd0e0cff9cecd52bbd94959cbaeab0b3497a718
Ken Ohkawa <knohkw@gmail.com>
2023-01-20 23:56:39 +0900

    fix: pass only one argument to parseInt

--- src/graph.ts
+++ src/graph.ts
@@ -8,16 +8,21 @@ export class Graph {
 }
 
 export function convertToGraph(str: string): Graph {
-    let lines = str.split('\n');
-    let nm = lines[0].split(' ').map(parseInt);
+    let lines = str.trim().split('\n');
+    let nm = lines[0].split(' ').map(s => parseInt(s));
     let adj = new Array(nm[0]);
     for (let i = 0; i < nm[0]; i++) {
         adj[i] = [];
     }
     for (let i = 1; i < lines.length; i++) {
-        let line = lines[i].split(' ').map(parseInt);
+        let line = lines[i].split(' ').map(s => parseInt(s));
+        console.log(line);
         let x = line[0];
         let y = line[1];
+        if (x >= adj.length || y >= adj.length) {
+            console.log("invalid edge from: " + x + ", to: " + y);
+            continue;
+        }
         adj[x].push(y);
         adj[y].push(x);
     }
