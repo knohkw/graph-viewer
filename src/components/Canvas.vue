bd20f88ae16f14487522f1e81b2a599b9198deca
Ken Ohkawa <knohkw@gmail.com>
2023-01-21 18:21:59 +0900

    feat: add edge to sample graph

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -12,7 +12,13 @@
             { id: 3, label: "Node 3" },
             { id: 4, label: "Node 4" },
             { id: 5, label: "Node 5" },]);
-        let edges = new DataSet([]);
+        let edges = new DataSet([
+            { from: 1, to: 3 },
+            { from: 1, to: 2 },
+            { from: 2, to: 4 },
+            { from: 2, to: 5 },
+            { from: 3, to: 3 },
+        ]);
 
       let container = document.getElementById("graphCanvas");
       if (!container) {
